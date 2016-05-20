e8b94746d8680d09bdba47242e5361499fc8a620
Taimoor Rana <taimoorrana1@hotmail.com>
2016-05-19 21:31:24 -0400

    GLFW Added

--- CentralPark/CentralPark.cpp
+++ CentralPark/CentralPark.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include <GLFW\glfw3.h>
+#include <iostream>
 
 
 int main()
 {
+	int x;
+	std::cout << "hello";
+	std::cin >> x;
+	std::cout << "hello";
     return 0;
 }
 
