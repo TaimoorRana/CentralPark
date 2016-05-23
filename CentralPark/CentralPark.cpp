f76909442fe1084613696c743758531379b63afd
thuynhse <thuynhse@outlook.com>
2016-05-23 18:28:20 -0400

    model work, view & projection does not work-> image disapear

--- CentralPark/CentralPark.cpp
+++ CentralPark/CentralPark.cpp
@@ -26,7 +26,7 @@ void Do_Movement();
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(0.0f, 0.0f, 3.0f, 0.0f, 1.0f, 0.0f, 45.0f, 45.0f);
 bool keys[1024];
 GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
@@ -38,6 +38,10 @@ GLfloat lastFrame = 0.0f;
 int main()
 {
 	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return 1;
+	}
 	// Init GLFW
 	glfwInit();
 	// Set all the required options for GLFW
@@ -57,6 +61,8 @@ int main()
 	glfwMakeContextCurrent(window);
 	// Set the required callback functions
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
@@ -66,11 +72,10 @@ int main()
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
+	glewInit();
 
 	// Define the viewport dimensions
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+	glViewport(0, 0, WIDTH, HEIGHT);
 	
 	// enable the depth test
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
@@ -82,16 +87,16 @@ int main()
 	// floor object
 	GLfloat floorVertices[] = {
 		// x, y , z
-		//-0.4f, 0.0f, 0.6f,
-		//0.4f, 0.0f, -0.6f,
-		//-0.4f, 0.0f, -0.6f,
-		//-0.4f, 0.0f, 0.6f,
-		//0.4f, 0.0f, 0.6f,
-		//0.4f, 0.0f, -0.6f
+		-0.4f, 0.0f, 0.6f,
+		0.4f, 1.0f, -0.6f,
+		-0.4f, 1.0f, -0.6f,
+		-0.4f, 0.0f, 0.6f,
+		0.4f, 0.0f, 0.6f,
+		0.4f, 1.0f, -0.6f
 		//test triangle
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		//-0.5f, -0.5f, 0.0f,
+		//0.5f, -0.5f, 0.0f,
+		//0.0f,  0.5f, 0.0f
 	};
 
 	GLuint VBO, VAO;
@@ -99,7 +104,6 @@ int main()
 	glGenBuffers(1, &VBO);
 	// Bind our Vertex Array Object first, then bind and set our buffers and pointers.
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
 
@@ -122,28 +126,28 @@ int main()
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.94f, 0.97f, 1.0f, 1.0f); // pale blue background color
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw our floor triangle
 		ourShader.use();
 		// Create camera transformation
-		glm::mat4 view;
-		view = camera.GetViewMatrix();
-		glm::mat4 projection;
-		projection = glm::perspective(camera.Zoom, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+		//glm::mat4 view;
+		//view = camera.GetViewMatrix();
+		//glm::mat4 projection;
+		//projection = glm::perspective(camera.Zoom, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 		// Get the uniform locations
-		GLint modelLoc = glGetUniformLocation(ourShader.program, "model");
-		GLint viewLoc = glGetUniformLocation(ourShader.program, "view");
-		GLint projLoc = glGetUniformLocation(ourShader.program, "projection");
+		
+		//GLint viewLoc = glGetUniformLocation(ourShader.program, "view");
+		//GLint projLoc = glGetUniformLocation(ourShader.program, "projection");
 		// Pass the matrices to the shader
 		glm::mat4 model;
+		GLint modelLoc = glGetUniformLocation(ourShader.program, "model");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-		// bind vao
-		glBindVertexArray(VAO); 
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		//glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		
+		glBindVertexArray(VAO); // bind vao
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0); //unbind vao
 
 		// Swap the screen buffers
@@ -151,6 +155,8 @@ int main()
 	}
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return 0;
 }
--- CentralPark/TextFiles/floorVertex.shader
+++ CentralPark/TextFiles/floorVertex.shader
@@ -6,5 +6,6 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main(){
-gl_Position = projection * view * model * vec4(position, 1.0f);
+	gl_Position = model * vec4(position, 1.0f);
+	//gl_Position = projection * view * model * vec4(position, 1.0f);
 }
