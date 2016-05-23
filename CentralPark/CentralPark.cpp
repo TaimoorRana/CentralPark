d3ff34130f9533d418b4bfd5d0db65467599f33f
thuynhse <thuynhse@outlook.com>
2016-05-23 14:35:03 -0400

    add camera & floor but doesnt show up

--- CentralPark/CentralPark.cpp
+++ CentralPark/CentralPark.cpp
@@ -1,26 +1,38 @@
-// Other headers
-#include "stdafx.h"
-
-#include <iostream>
-//#include <Windows.h>
+// some code snippets in this source file are from learnopengl.com
 
 // GLEW
 //#define GLEW_STATIC
 #include <GL/glew.h>
-
 // GLFW
 #include <GLFW/glfw3.h>
+// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+// Other headers
+#include <iostream>
+#include "stdafx.h"
+#include "Camera.h"
 #include "Shader.h"
 
-//test2
+
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void Do_Movement();
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
@@ -59,17 +71,80 @@ int main()
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
+	
+	// enable the depth test
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // selected the type of testing: smaller value as closer to view
 
+	// Setup and compile shaders
+	Shader ourShader("../CentralPark/TextFiles/floorVertex.shader", "../CentralPark/TextFiles/floorFragment.shader");
+
+	// floor object
+	GLfloat floorVertices[] = {
+		// x, y , z
+		//-0.4f, 0.0f, 0.6f,
+		//0.4f, 0.0f, -0.6f,
+		//-0.4f, 0.0f, -0.6f,
+		//-0.4f, 0.0f, 0.6f,
+		//0.4f, 0.0f, 0.6f,
+		//0.4f, 0.0f, -0.6f
+		//test triangle
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Bind our Vertex Array Object first, then bind and set our buffers and pointers.
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0); // Unbind VAO
+	
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Set frame time for smooth camera movement
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 
 		// Render
 		// Clear the colorbuffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.94f, 0.97f, 1.0f, 1.0f); // pale blue background color
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw our floor triangle
+		ourShader.use();
+		// Create camera transformation
+		glm::mat4 view;
+		view = camera.GetViewMatrix();
+		glm::mat4 projection;
+		projection = glm::perspective(camera.Zoom, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(ourShader.program, "model");
+		GLint viewLoc = glGetUniformLocation(ourShader.program, "view");
+		GLint projLoc = glGetUniformLocation(ourShader.program, "projection");
+		// Pass the matrices to the shader
+		glm::mat4 model;
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		// bind vao
+		glBindVertexArray(VAO); 
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0); //unbind vao
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
@@ -80,10 +155,58 @@ int main()
 	return 0;
 }
 
+
+// implementation of prototype functions
+// Is called whenever a key is pressed/released via GLFW
+// Moves/alters the camera positions based on user input
+void Do_Movement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W])
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-	std::cout << key << std::endl;
+	//cout << key << endl;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
